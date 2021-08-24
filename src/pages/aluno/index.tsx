c45c7d5aac256e0a5ea74f7701d95be7ac08aeb8
Marcio Fernandes <farciomernandes@gmail.com>
2021-08-24 12:53:25 -0300

    projeto iniciado, signin aluno criado

---
+++ src/pages/aluno/index.tsx
@@ -0,0 +1,81 @@
+import { Button, Flex, Input, Stack, Text } from '@chakra-ui/react';
+import Image from 'next/image'
+
+import logoImg from '../../assets/images/logo.svg';
+
+export default function Home() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center">
+      <Flex as="form" w="100%" maxWidth={520}
+        bg="white.900"
+        p="50"
+        py="100"
+        borderRadius={15}
+        flexDirection="column"
+      >
+        <Flex
+          flexDirection="column"
+          align="center"
+          justify="center"
+          mb="20"
+        >
+          <Stack spacing="12">
+            <Image 
+            src={logoImg} 
+            alt="E- Test" 
+            width={30}
+            height={30}
+            />
+
+            <Text
+            fontSize="3xl" 
+            color="black"
+            align="center"
+            justify="center"
+            >
+              Entrar como aluno
+            </Text>
+          </Stack>
+        </Flex>
+
+        <Stack spacing="4">
+
+          <Input
+            name="matricula"
+            type="text"
+            focusBorderColor="purple.800"
+            variant="outline"
+            _hover={{
+              bgColor: "white.900"
+            }}
+            size="lg"
+          />
+          <Input
+            name="senha"
+            type="password"
+            focusBorderColor="purple.800"
+            variant="outline"
+            _hover={{
+              bgColor: "white.900"
+            }}
+            size="lg"
+          />
+        </Stack>
+
+        <Stack spacing="5">
+          <Button type="submit" mt="6" size="lg" colorScheme="red">Entrar</Button>
+
+          <Flex justify="center" align="center" fontWeight="bold">
+            <Text color="black">Não tem conta?</Text>  
+            <Text color="purple.800" ml="2">Cadastre-se</Text> 
+          </Flex>
+        </Stack>
+
+      </Flex>
+    </Flex>
+  )
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+import { extendTheme } from  '@chakra-ui/react';
+
+export const theme = extendTheme({
+    colors: {
+        white: {
+            "900": "#FFFFFF",
+            "700": "#FEFEFE",
+            "600": "#F7F9FF"
+        },
+        purple: {
+            "800": "#5A17E9",
+            "500": "#4813BA"
+        },
+        red: {
+            "500": "#E53C17"
+        },
+        green: {
+            "700": "##09C55E"
+        }
+    },
+    styles: {
+        global: {
+            body:{
+                bg: 'purple.800',
+                color: 'white.900'
+            }
+        }
+    }
+})
