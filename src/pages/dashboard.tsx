5b7f93a8d1b3128b99cd30407705285225dd69b6
Marcio Fernandes <farciomernandes@gmail.com>
2021-08-25 00:09:31 -0300

    tela dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Icon, Stack, Text, VStack } from '@chakra-ui/react';
 import Image from 'next/image'
 
 import dashboardImg from '../assets/images/dashboardIMG.svg';
@@ -10,20 +10,73 @@ export default function Dashboard() {
       <Header />
 
       <Flex w="100%" my="6" justify="space-around" align="center">
-        <Flex>
+        <Flex mr="auto" mt="20" flexDir="column" maxWidth={720}>
+
+          <Text
+            fontSize="7xl"
+          >
+            Bem vindo ao E-test,
+            <Text fontWeight="bold"> Marcio</Text>
+          </Text>
+
+          <Text mt="5">
+            Entre nas suas turmas e veja todos os comentários feitos
+            pelos professores, confira já suas notas através do boletim,
+            veja suas avaliações respondidas
+          </Text>
+
+
+          <Flex
+            align="center"
+            justify="space-between"
+            mt="12"
+          >
+
+            <VStack spacing="8">
+              <Button w={350}  type="submit" h="57" size="lg" bg="white.900" color="purple.800"
+              >
+                Minhas turmas
+              </Button>
+
+              <Button w={350}  type="submit" h="57" size="lg" bg="white.900"  color="purple.800"
+              >Minhas avaliações</Button>
+            </VStack>
+
+
+            <VStack spacing="8">
+              <Button w={350}  type="submit" h="57" size="lg" colorScheme="green"
+              >
+                Meu Boletim
+              </Button>
+
+              <Button w={350}  type="submit" h="57" size="lg" colorScheme="red"
+              >Editar perfil</Button>
+            </VStack>
+
+          </Flex>
 
         </Flex>
         <Flex>
-          
-        <Box
-        position="absolute"
-        >
-          <Image src={dashboardImg} width={1200} height={1200} />
-        </Box>
+          <Box
+            position="absolute"
+            right="0"
+            top="20"
+          >
+            <Image src={dashboardImg} width={900} height={900} />
+          </Box>
 
         </Flex>
       </Flex>
 
+    <Flex as="footer"
+     position="absolute" bottom="0" 
+     left="30%"
+    >
+
+    <Text>
+      Copyright ©2021 All rights reserved  | This template is made with by Marcio Fernandes and Henrique Benício
+    </Text>
+    </Flex>
     </Flex>
   )
 }
